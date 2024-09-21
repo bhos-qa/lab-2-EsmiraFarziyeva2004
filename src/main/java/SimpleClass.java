be2637e3743447b12832a4a37f4b2f4b4428c378
EsmiraFarziyeva2004 <123585048+EsmiraFarziyeva2004@users.noreply.github.com>
2024-09-21 22:38:01 +0400

    Test

--- src/main/java/SimpleClass.java
+++ src/main/java/SimpleClass.java
@@ -7,6 +7,6 @@ public class SimpleClass {
 
     public static void main(String[] args) {
         SimpleClass sc = new SimpleClass();
-        System.out.println("Sum: " + sc.sum(9, 10));
+        System.out.println("Sum: " + sc.sum(2, 10));
     }
 }
--- src/test/java/SimpleClassTest.java
+++ src/test/java/SimpleClassTest.java
@@ -6,7 +6,7 @@ public class SimpleClassTest {
     @Test
     public void testSum() {
         SimpleClass sc = new SimpleClass();
-        int result = sc.sum(9, 10);
-        assertEquals(19, result, "Sum of 9 and 10 should be 19.");
+        int result = sc.sum(2, 10);
+        assertEquals(12, result, "Sum of 2 and 10 should be 12.");
     }
 }
