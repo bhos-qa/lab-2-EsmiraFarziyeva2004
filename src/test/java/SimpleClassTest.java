a69617b15fb2f8494cfaa07a11459da3f0bad51c
EsmiraFarziyeva2004 <123585048+EsmiraFarziyeva2004@users.noreply.github.com>
2024-09-21 19:18:59 +0400

    My commit

---
+++ src/test/java/SimpleClassTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SimpleClassTest {
+
+    @Test
+    public void testSum() {
+        SimpleClass sc = new SimpleClass();
+        int result = sc.sum(5, 10);
+        assertEquals(15, result, "Sum of 5 and 10 should be 15");
+    }
+}
