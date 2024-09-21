2a4cedd8991e6dabf5e70a9292e027c41dc7579d
EsmiraFarziyeva2004 <123585048+EsmiraFarziyeva2004@users.noreply.github.com>
2024-09-21 20:52:30 +0400

    lab2

--- src/test/java/SimpleClassTest.java
+++ src/test/java/SimpleClassTest.java
@@ -7,6 +7,6 @@ public class SimpleClassTest {
     public void testSum() {
         SimpleClass sc = new SimpleClass();
         int result = sc.sum(5, 10);
-        assertEquals(15, result, "Sum of 6 and 10 should be 16.");
+        assertEquals(15, result, "Sum of 7 and 10 should be 17.");
     }
 }
